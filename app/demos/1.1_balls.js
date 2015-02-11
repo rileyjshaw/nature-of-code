4939a14c27046a65f5f5fb9783668843ea3ebf0c
Riley Shaw <rileyjshaw@gmail.com>
2015-02-11 15:53:57 -0500

    Convert indentation to spaces

--- app/demos/1.1_balls.js
+++ app/demos/1.1_balls.js
@@ -1,31 +1,31 @@
 (function (exports) {
-	var Ball = nature.entityFactory(function step (scene) {
-		var pos = this.pos;
-		var x = pos.x;
-		var y = pos.y;
+  var Ball = nature.entityFactory(function step (scene) {
+    var pos = this.pos;
+    var x = pos.x;
+    var y = pos.y;
 
-		if (x < this.radius || x + this.radius > scene.width) {
-			this.velocity.x *= -1;
-		}
+    if (x < this.radius || x + this.radius > scene.width) {
+      this.velocity.x *= -1;
+    }
 
-		if (y < this.radius || y + this.radius > scene.height) {
-			this.velocity.y *= -1;
-		}
-	}, function init () {
-		var x = Math.random() * 2 - 1; // (-1, 1)
-		var y = Math.random() * 2 - 1; // (-1, 1)
-		var factor = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+    if (y < this.radius || y + this.radius > scene.height) {
+      this.velocity.y *= -1;
+    }
+  }, function init () {
+    var x = Math.random() * 2 - 1; // (-1, 1)
+    var y = Math.random() * 2 - 1; // (-1, 1)
+    var factor = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
 
-		this.phase = Math.floor(Math.random() * 360);
-		this.velocity = new nature.Vector(x / factor, y / factor);
-	}, {radius: 90, speed: 14});
+    this.phase = Math.floor(Math.random() * 360);
+    this.velocity = new nature.Vector(x / factor, y / factor);
+  }, {radius: 90, speed: 14});
 
-	new nature.Scene(function step (scene) {
-		scene.ctx.clearRect(0, 0, scene.width, scene.height);
-	}, function init (scene) {
-		scene.entities.push(new Ball(scene, scene.width / 2, scene.height / 2));
-		scene.entities.push(new Ball(scene, scene.width / 4, scene.height / 4));
-		scene.entities.push(new Ball(scene, scene.width * 3 / 4, scene.height * 3 / 4));
-	});
+  new nature.Scene(function step (scene) {
+    scene.ctx.clearRect(0, 0, scene.width, scene.height);
+  }, function init (scene) {
+    scene.entities.push(new Ball(scene, scene.width / 2, scene.height / 2));
+    scene.entities.push(new Ball(scene, scene.width / 4, scene.height / 4));
+    scene.entities.push(new Ball(scene, scene.width * 3 / 4, scene.height * 3 / 4));
+  });
 
 })(typeof nature === 'object' ? nature : (nature = {}));
--- app/static/util.js
+++ app/static/util.js
@@ -1,7 +1,7 @@
 (function (exports) {
-	var PI2 = Math.PI * 2;
+  var PI2 = Math.PI * 2;
 
-	function radians (deg) {
+  function radians (deg) {
     return deg / 360 * 2 * Math.PI;
   }
 
@@ -13,157 +13,157 @@
     return Math.cos(radians(deg));
   }
 
-	var height = window.innerHeight;
-	var width = document.documentElement.clientWidth;
+  var height = window.innerHeight;
+  var width = document.documentElement.clientWidth;
 
-	function createCanvas () {
-		var canvas = document.createElement('canvas');
-		var ctx = canvas.getContext('2d');
+  function createCanvas () {
+    var canvas = document.createElement('canvas');
+    var ctx = canvas.getContext('2d');
 
-		// Creates a dummy canvas to test device's pixel ratio
-		ratio = (function () {
-			var ctx = document.createElement('canvas').getContext('2d');
-			var dpr = window.devicePixelRatio || 1;
-			var bsr = ctx.webkitBackingStorePixelRatio ||
-			          ctx.mozBackingStorePixelRatio ||
-			          ctx.msBackingStorePixelRatio ||
-			          ctx.oBackingStorePixelRatio ||
-			          ctx.backingStorePixelRatio || 1;
-			return dpr / bsr;
-		})();
+    // Creates a dummy canvas to test device's pixel ratio
+    ratio = (function () {
+      var ctx = document.createElement('canvas').getContext('2d');
+      var dpr = window.devicePixelRatio || 1;
+      var bsr = ctx.webkitBackingStorePixelRatio ||
+                ctx.mozBackingStorePixelRatio ||
+                ctx.msBackingStorePixelRatio ||
+                ctx.oBackingStorePixelRatio ||
+                ctx.backingStorePixelRatio || 1;
+      return dpr / bsr;
+    })();
 
-		canvas.width = width * ratio;
-		canvas.height = height * ratio;
-		canvas.style.width = width + 'px';
-		canvas.style.height = height + 'px';
+    canvas.width = width * ratio;
+    canvas.height = height * ratio;
+    canvas.style.width = width + 'px';
+    canvas.style.height = height + 'px';
 
-		document.body.appendChild(canvas);
-		return canvas;
-	}
+    document.body.appendChild(canvas);
+    return canvas;
+  }
 
-	function Vector (x, y) {
-		this.x = x;
-		this.y = y;
-	}
+  function Vector (x, y) {
+    this.x = x;
+    this.y = y;
+  }
 
-	Vector.prototype.plus = function (vector) {
-		var x = this.x + vector.x;
-		var y = this.y + vector.y;
-		return new Vector(x, y);
-	};
+  Vector.prototype.plus = function (vector) {
+    var x = this.x + vector.x;
+    var y = this.y + vector.y;
+    return new Vector(x, y);
+  };
 
-	Vector.prototype.times = function (factor) {
-		var x = this.x * factor;
-		var y = this.y * factor;
-		return new Vector(x, y);
-	};
+  Vector.prototype.times = function (factor) {
+    var x = this.x * factor;
+    var y = this.y * factor;
+    return new Vector(x, y);
+  };
 
-	function BaseEntity () {
-		this.phase = 0;
-		this.velocity = new Vector(0, 0);
-		this.acceleration = new Vector(0, 0);
-		this.pos = new Vector(0, 0);
-		this.radius = 2;
-		this.speed = 1;
-	}
+  function BaseEntity () {
+    this.phase = 0;
+    this.velocity = new Vector(0, 0);
+    this.acceleration = new Vector(0, 0);
+    this.pos = new Vector(0, 0);
+    this.radius = 2;
+    this.speed = 1;
+  }
 
-	BaseEntity.prototype.draw = function (scene) {
-		var ctx = scene.ctx;
-		ctx.fillStyle = ctx.strokeStyle = 'hsl(' + (scene.age + this.phase) + ', 55%, 70%)';
-		ctx.beginPath();
-		ctx.arc(this.pos.x, this.pos.y, this.radius, 0, PI2);
-		ctx.closePath();
-		ctx.fill();
-	};
+  BaseEntity.prototype.draw = function (scene) {
+    var ctx = scene.ctx;
+    ctx.fillStyle = ctx.strokeStyle = 'hsl(' + (scene.age + this.phase) + ', 55%, 70%)';
+    ctx.beginPath();
+    ctx.arc(this.pos.x, this.pos.y, this.radius, 0, PI2);
+    ctx.closePath();
+    ctx.fill();
+  };
 
-	BaseEntity.prototype.die = function (scene) {
-		var entities = scene.entities;
-		return entities.splice(entities.indexOf(this), 1);
-	};
+  BaseEntity.prototype.die = function (scene) {
+    var entities = scene.entities;
+    return entities.splice(entities.indexOf(this), 1);
+  };
 
-	function entityFactory (stepFn, init, protoHash) {
-		var Entity;
+  function entityFactory (stepFn, init, protoHash) {
+    var Entity;
 
-		if (init) {
-			Entity = function (scene, x, y) {
-				this.pos = new Vector(x, y);
-				init.call(this, scene);
-			};
-		} else {
-			Entity = function (scene, x, y) {
-				this.pos = new Vector(x, y);
-			};
-		}
+    if (init) {
+      Entity = function (scene, x, y) {
+        this.pos = new Vector(x, y);
+        init.call(this, scene);
+      };
+    } else {
+      Entity = function (scene, x, y) {
+        this.pos = new Vector(x, y);
+      };
+    }
 
-		Entity.prototype = new BaseEntity();
-		Entity.prototype.constructor = Entity;
+    Entity.prototype = new BaseEntity();
+    Entity.prototype.constructor = Entity;
 
-		if (protoHash) {
-			Object.keys(protoHash).forEach(function (key) {
-				Entity.prototype[key] = protoHash[key];
-			});
-		}
+    if (protoHash) {
+      Object.keys(protoHash).forEach(function (key) {
+        Entity.prototype[key] = protoHash[key];
+      });
+    }
 
-		if (typeof stepFn !== 'function') {
-			stepFn = function () {};
-		}
+    if (typeof stepFn !== 'function') {
+      stepFn = function () {};
+    }
 
-		Entity.prototype.step = function step (scene) {
-			var x = this.pos.x, y = this.pos.y;
+    Entity.prototype.step = function step (scene) {
+      var x = this.pos.x, y = this.pos.y;
 
-			stepFn.call(this, scene);
-			this.velocity = this.velocity.plus(this.acceleration);
-			this.pos = this.pos.plus(this.velocity.times(this.speed));
+      stepFn.call(this, scene);
+      this.velocity = this.velocity.plus(this.acceleration);
+      this.pos = this.pos.plus(this.velocity.times(this.speed));
 
-			if (x < -this.radius * 2 || x > scene.width ||
-			    y < -this.radius * 2 || y > scene.height) {
-				typeof this.outOfBounds === 'function' ?
-					this.outOfBounds(scene) :
-					this.die(scene);
-			} else {
-				this.draw(scene);
-			}
-		};
+      if (x < -this.radius * 2 || x > scene.width ||
+          y < -this.radius * 2 || y > scene.height) {
+        typeof this.outOfBounds === 'function' ?
+          this.outOfBounds(scene) :
+          this.die(scene);
+      } else {
+        this.draw(scene);
+      }
+    };
 
-		return Entity;
-	}
+    return Entity;
+  }
 
-	function Scene (stepFn, init, entityCap) {
-		var self = this;
-		var canvas = createCanvas();
+  function Scene (stepFn, init, entityCap) {
+    var self = this;
+    var canvas = createCanvas();
 
-		stepFn = stepFn || function () {};
+    stepFn = stepFn || function () {};
 
-		function loop () {
-			var entities = self.entities;
-			entities.splice(0, entities.length - self.entityCap);
-			stepFn(self);
-			entities.forEach(function (entity) {
-				entity.step(self);
-			});
-			self.age++;
-			window.requestAnimationFrame(loop);
-		};
+    function loop () {
+      var entities = self.entities;
+      entities.splice(0, entities.length - self.entityCap);
+      stepFn(self);
+      entities.forEach(function (entity) {
+        entity.step(self);
+      });
+      self.age++;
+      window.requestAnimationFrame(loop);
+    };
 
-		self.age = 0;
-		self.entities = [];
-		self.canvas = canvas;
-		self.ctx = canvas.getContext('2d');
-		self.height = canvas.height;
-		self.width = canvas.width;
+    self.age = 0;
+    self.entities = [];
+    self.canvas = canvas;
+    self.ctx = canvas.getContext('2d');
+    self.height = canvas.height;
+    self.width = canvas.width;
 
-		if (init) init(self);
-		if (entityCap) self.entityCap = entityCap;
+    if (init) init(self);
+    if (entityCap) self.entityCap = entityCap;
 
-		window.requestAnimationFrame(loop);
-	}
+    window.requestAnimationFrame(loop);
+  }
 
-	Scene.prototype.entityCap = Infinity;
+  Scene.prototype.entityCap = Infinity;
 
-	exports.radians = radians;
-	exports.sin = sin;
-	exports.cos = cos;
-	exports.Vector = Vector;
-	exports.entityFactory = entityFactory;
-	exports.Scene = Scene;
+  exports.radians = radians;
+  exports.sin = sin;
+  exports.cos = cos;
+  exports.Vector = Vector;
+  exports.entityFactory = entityFactory;
+  exports.Scene = Scene;
 })(typeof nature === 'object' ? nature : (nature = {}));
